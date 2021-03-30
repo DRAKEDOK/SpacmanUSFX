91a9810c5bd85a41692ecb9a9372f3cd1482ac15
DRAKEDOK <armando.jagv79@gmail.com>
2021-03-30 18:34:37 -0400

    primera prueba

---
+++ pacmanUSFX/main basic.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << " Main de prueba " << endl;
+	return 0;
+
+}
---
+++ pacmanUSFX/pacman.h
@@ -0,0 +1,31 @@
+#pragma once
+class pacman
+{
+private:
+	float posicionX;
+	float posicionY;
+	int color;
+	float velocidadX;
+	float velocidadY;
+public:
+	void move(float _velocidadX, float _velocidadY)
+};
+
+Clases
+------
+
+Pacman
+Fantasma
+Moneda
+Frutita
+Muro
+Contador de puntos
+Nivel
+Contador de vidas
+Contador de tiempo
+Super moneda
+Laberinto
+Habilidad
+Generador fantasmas
+Menú
+Game manager
