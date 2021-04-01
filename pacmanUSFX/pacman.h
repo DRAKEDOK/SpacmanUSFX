724b09315be6ad6809e13c222542ec9be85919f2
DRAKEDOK <armando.jagv79@gmail.com>
2021-04-01 12:10:23 -0400

    xD

--- pacmanUSFX/pacman.h
+++ pacmanUSFX/pacman.h
@@ -9,29 +9,16 @@ private:
 	float velocidadY;
 public:
 	void move(float _velocidadX, float _velocidadY);
+
+	float getPosicionX() { return posicionX}
+	void setPosicionX(float _posicionX) { posicionX = _posicionX;  }
+	float getPosicionY() { return posicionY }
+	void setPosicionY(float _posicionY) { posicionY = _posicionY; }
+
+	float getVelocidadX() { return VelocidadX }
+	void setVelocidadX(float _velocidadX) { velocidadX = _velocidadX; }
+	float getVelocidadY() { return VelocidadY }
+	void setVelocidadY(float _velocidadY) { velocidadY = _velocidadY; }
+
 };
 
-
-
-class pacman {
-public:
-	int x, y;
-	bool cc;
-	direccion dir;
-	pacman(int xx, int yy, direccion d) { x = xx; y = yy; dir = d; }
-	void dibujar();
-};
-
-
-class fantasma {
-public:
-	int x, y;
-	int color;
-	bool levantado; //si pacman es invencible y lo levanto
-	fantasma(int xx, int yy, int c) { x = xx; y = yy; color = c; levantado = false; }
-	void direccionar();
-	void randomizar_();
-	void dibujar();
-};
-
-fuen
